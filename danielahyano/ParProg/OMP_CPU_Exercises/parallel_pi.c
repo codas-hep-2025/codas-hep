f07b8fbf0022376921329da9d6090e2f78ff666c
danielahyano <danielahyano@gmail.com>
2025-07-22 15:45:42 +0000

    parallel_pi now working!

--- danielahyano/ParProg/OMP_CPU_Exercises/parallel_pi.c
+++ danielahyano/ParProg/OMP_CPU_Exercises/parallel_pi.c
@@ -16,46 +16,40 @@ History: Written by Tim Mattson, 11/99.
 #include <stdio.h>
 #include <omp.h>
 static long num_steps = 100000000;
+// static long num_steps = 10;
 double step;
+#define NUM_THREADS 2
 int main ()
 {
-	  int i;
-	  double pi, sum = 0.0;
+	  int i, numthrds;
+	  double pi, sum[NUM_THREADS];
 	  double start_time, run_time;
-
 	  step = 1.0/(double) num_steps;
         	 
 	  start_time = omp_get_wtime();
-      int num_threads = 4;
-      double sums_list[num_threads];
 
-      omp_set_num_threads(num_threads);
+      omp_set_num_threads(NUM_THREADS);
       #pragma omp parallel
       {
-          int current_thread = omp_get_thread_num();
-          printf("%d ", current_thread);
-          double partial_sum = 0.0;
+          int i, id, nthreads;
+          double x;
+          
+          id = omp_get_thread_num();
+          nthreads = omp_get_num_threads();
+          if (id == 0)  numthrds = nthreads;
+          for (i=id, sum[id]=0.0; i < num_steps; i=i+nthreads ){
+              x = (i-0.5)*step;
+    		  sum[id] = sum[id] + 4.0/(1.0+x*x);
+          }
 
-          int start_index = (num_steps/num_threads) * current_thread + 1;
-          int end_index = start_index + (num_steps/num_threads); 
-          double x; 
-          for (i=start_index;i<= end_index; i++){
-    		  x = (i-0.5)*step;
-    		  partial_sum = partial_sum + 4.0/(1.0+x*x);
-    	  }
-           
-          printf("partial: %d ", partial_sum);
-          sums_list[current_thread] = partial_sum;
       }
 
-      for (i=1;i<= num_threads; i++){
-        sum += sums_list[i]; // Add the current element to sum
+      for (i=0, pi=0.0; i < numthrds; i++){
+        pi += sum[i]*step;
       }
-      
-	  pi = step * sum; 
-      printf("%d", pi);
-	  // run_time = omp_get_wtime() - start_time;
-	  // printf("\n pi with %ld steps is %lf in %lf seconds\n ",num_steps,pi,run_time);
+          
+	  run_time = omp_get_wtime() - start_time;
+	  printf("\n pi with %ld steps is %lf in %lf seconds\n ",num_steps,pi,run_time);
 }	  
 
 
